25704a2ebf8f796e900c02e1e4880826cbdd4b8b
FuadMannan <fuad.mannan@gmail.com>
2017-07-11 16:03:11 -0400

    Added private list powers and private method initialize.

--- Lesson9A/SuperHuman.cs
+++ Lesson9A/SuperHuman.cs
@@ -6,7 +6,7 @@ using System.Text;
 /* Name: Fuad Mannan
  * Date: July 11, 2017
  * Description: This is the SuperHuman sub class.
- * Version: 0.1 - Created the SuperHuman sub class.
+ * Version: 0.2 - Added private initialize method.
  */
 
 namespace Lesson9A
@@ -18,13 +18,22 @@ namespace Lesson9A
     public class SuperHuman : Human
     {
         //Private Fields
-
+        private List<Power> _powers;
         //Public Properties
 
         //Constructors
         public SuperHuman(string name) : base(name)
         {
+            this._initialize();
+        }
 
+        //Private methods
+        /// <summary>
+        /// This method initializes and assigns default values to Class fields.
+        /// </summary>
+        private void _initialize()
+        {
+            this._powers = new List<Power>();
         }
 
         //Public Methods
