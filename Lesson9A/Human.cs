a801cce19c189db2209da1ba3cbda0b85166e58e
300923068 <300923068@PR-A315-08>
2017-07-11 15:48:31 -0400

    Add project files.

---
+++ Lesson9A/Human.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Fuad Mannan
+ * Date: July 11, 2017
+ * Description: This is the abstract class Human which other subclasses
+ * will derive from.
+ * Version: 0.1 - Class created.
+ */
+
+namespace Lesson9A
+{
+    /// <summary>
+    /// This is the Human Abstract Class
+    /// </summary>
+    public abstract class Human
+    {
+        //Private Fields (Instance Variables)
+        private string _name;
+        //Public Properties
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                this._name = value;
+            }
+        }
+        //Constructors
+        /// <summary>
+        /// This is the constructor for the Human abstract class.
+        /// It requires one parameter - name (string).
+        /// </summary>
+        /// <param name="name"></param>
+        public Human(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ Lesson9A/SuperHuman.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/* Name: Fuad Mannan
+ * Date: July 11, 2017
+ * Description: This is the SuperHuman sub class.
+ * Version: 0.1 - Created the SuperHuman sub class.
+ */
+
+namespace Lesson9A
+{
+    /// <summary>
+    /// This is the SuperHuman sub clas.
+    /// It inherits from the Human Superclass.
+    /// </summary>
+    public class SuperHuman : Human
+    {
+        //Private Fields
+
+        //Public Properties
+
+        //Constructors
+        public SuperHuman(string name) : base(name)
+        {
+
+        }
+
+        //Public Methods
+    }
+}
